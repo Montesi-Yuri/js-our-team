0bb53ae63ece44dcda075bf2d68bf16182744c92
Montesi-Yuri <yuri13312@gmail.com>
2023-06-26 18:50:16 +0200

    ADD - images and style (BONUS)

--- js/script.js
+++ js/script.js
@@ -4,51 +4,68 @@
 
 const teamMembers = [
     {
-       name : 'Wayne Bernett',
-       role : 'Founder & CEO',
-       image : '../img/wayne-barnett-founder-ceo.jpg'
+        image : 'img/wayne-barnett-founder-ceo.jpg',
+        name : 'Wayne Bernett',
+        role : 'Founder & CEO'
     },
     {
+        image : 'img/angela-caroll-chief-editor.jpg',
         name : 'Angela Caroll',
-        role : 'Chief Editor',
-        image : '../img/angela-caroll-chief-editor.jpg'
+        role : 'Chief Editor'
     },
     {
+        image : 'img/walter-gordon-office-manager.jpg',
         name : 'Walter Gordon',
-        role : 'Office Manager',
-        image : '../img/walter-gordon-office-manager.jpg'
+        role : 'Office Manager'
     },
     {
+        image : 'img/angela-lopez-social-media-manager.jpg',
         name : 'Angela Lopez',
         role : 'Social Media Manager',
-        image : '../img/angela-lopez-social-media-manager.jpg'
     },
     {
+        image : 'img/scott-estrada-developer.jpg',
         name : 'Scott Estrada',
-        role : 'Developer',
-        image : 'img/scott-estrada-developer.jpg'
+        role : 'Developer'
     },
     {
+        image : 'img/barbara-ramos-graphic-designer.jpg',
         name : 'Barbara Ramos',
-        role : 'Graphic Designer',
-        image : '../img/barbara-ramos-graphic-designer.jpg'
+        role : 'Graphic Designer'
     }
 ];
+
 
 for (let i = 0; i < teamMembers.length; i++) {
     const member = teamMembers[i];
     console.log('member', member, typeof member);
     const card = document.createElement('div');
-    card.classList.add('col-4', 'mb-3');
+    card.classList.add('col-auto', 'mb-3', 'row', 'card', 'm-3', 'text-bg-dark');
     const cardContainer = document.getElementById('card-container');
     
     cardContainer.append(card);
     
     for (const key in member) {
-        console.log(key, member[key]);
-        card.append(member[key]);
+        console.log('key', key, typeof key);
+        console.log('member[key]',member[key], typeof member[key]);
+        const cardRow = document.createElement('div');
+        cardRow.classList.add('col-12', 'text-center', 'card-body');
+        cardRow.append(member[key]);
+        card.append(cardRow);
+
+        if( key == 'image'){
+            cardRow.innerHTML = '';
+            const imgCont = document.createElement('img');
+            imgCont.src = member[key];
+            console.log('IMG SRC', imgCont);
+            cardRow.classList.remove('card-body');
+            imgCont.classList.add('card-img-top', 'mt-2');
+            cardRow.append(imgCont);
+            
+        }
     }
 
     console.log('--------------------------------');
     
 }
+
