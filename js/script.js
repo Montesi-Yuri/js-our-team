0200be378f46e0556b996cf88c1db496dde5a1db
Montesi-Yuri <yuri13312@gmail.com>
2023-06-26 16:18:15 +0200

    ADD - members array JS

--- js/script.js
+++ js/script.js
@@ -2,3 +2,30 @@
     JS
 */
 
+const teamMembers = [
+    {
+       name : 'Wayne Bernett',
+       role : 'Founder & CEO',
+       image : '../img/wayne-barnett-founder-ceo.jpg'
+    }
+    {
+        name : 'Angela Caroll',
+        role : 'Chief Editor',
+        image : '../img/angela-caroll-chief-editor.jpg'
+    }
+    {
+        name : 'Walter Gordon',
+        role : 'Office Manager',
+        image : '../img/walter-gordon-office-manager.jpg'
+    }
+    {
+        name : 'Angela Lopez',
+        role : 'Social Media Manager',
+        image : '../img/angela-lopez-social-media-manager.jpg'
+    }
+    {
+        name : 'Barbara Ramos',
+        role : 'Graphic Designer',
+        image : '../img/barbara-ramos-graphic-designer.jpg'
+    }
+]
